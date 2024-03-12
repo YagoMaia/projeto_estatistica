d3918278d1beeac8c494f0a4f7a52d6406455b6a
Yago Maia <yagohenriquev123@gmail.com>
2024-03-12 14:27:33 -0300

    Adicionando comentários nos métodos da classe

--- metodos.py
+++ metodos.py
@@ -3,6 +3,9 @@ import math
 class MetodosMatematicos():
     
     def __init__(self, dados):
+        """
+        Iniciando a classe
+        """
         self.dados = dados
         self.tamanho_amostra = len(self.dados)
         self.media = self.calcular_media()
@@ -17,26 +20,44 @@ class MetodosMatematicos():
         self.li, self.lf = self.montar_listas_intervalos()
             
     def calcular_media(self):
+        """
+        Método Responsável por calcular a média
+        """
         media = sum(self.dados)/self.tamanho_amostra
         return media
     
     def calcular_mediana(self):
+        """
+        Método Responsável por calcular a mediana
+        """
         mediana = (self.dados[int(self.tamanho_amostra / 2)] + self.dados[int(self.tamanho_amostra / 2) + 1]) / 2 if self.tamanho_amostra % 2 == 0 else self.dados[int((self.tamanho_amostra - 1) / 2)]
         return mediana
     
     def calcular_amplitude(self):
+        """
+        Método responsável por calcular a amplitude
+        """
         amplitude = max(self.dados) - min(self.dados)
         return amplitude
         
     def calcular_k(self):
+        """
+        Método responsável por calcular o k
+        """
         k = round(math.sqrt(self.tamanho_amostra))
         return k
     
     def calcular_c(self):
+        """
+        Método responsável por calcular o c
+        """
         c = round(self.amplitude / (self.k - 1), 2)
         return c
     
     def calcular_variancia(self):
+        """
+        Método responsável por calcular a Variância
+        """
         soma = 0
         for valor in self.dados:
             soma += (self.media - valor) ** 2
@@ -44,10 +65,16 @@ class MetodosMatematicos():
         return variancia
     
     def calcular_limite_inferior(self):
+        """
+        Método responsável por calcular o limite inferior
+        """
         limite_inferior = min(self.dados) - self.c/2
         return limite_inferior
     
     def montar_listas_intervalos(self):
+        """
+        Método responsável por calcular os intervalos
+        """
         lista_intervalos_ini = []
         lista_intervalos_fim = []
         
@@ -58,10 +85,16 @@ class MetodosMatematicos():
         return lista_intervalos_ini, lista_intervalos_fim
     
     def printar_classes(self):
+        """
+        Método responsável por printar as classes baseado nos dados
+        """
         for key, value in self.dados_tabela.items():
             print(key, value)
             
     def montar_tabela(self):
+        """
+        Método responsável por criar a tabela
+        """
         fi = []
         fr = []
         fac = []
